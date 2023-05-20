20fbb761b303e28e2e3a16bf6c816a961a13f4a7
griga <grigach@gmail.com>
2023-05-20 19:12:01 +0300

    linked list

--- linked-list.go
+++ linked-list.go
@@ -16,11 +16,99 @@ type LinkedList struct {
 	Length int
 }
 
-func (ll LinkedList) String() string {
-	if ll.Head == nil {
+func (list *LinkedList) Prepend(value int) {
+	node := &ListNode{Value: value}
+	if list.Length == 0 {
+		list.Head, list.Tail = node, node
+	} else {
+		next := list.Head
+		next.Prev = node
+		node.Next = next
+		list.Head = node
+	}
+	list.Length++
+}
+
+func (list *LinkedList) InsertAt(idx int, v int) error {
+	if idx > list.Length {
+		return fmt.Errorf("can't InsertAt, idx %v is unavailable", idx)
+	} else if idx == list.Length {
+		list.Append(v)
+		return nil
+	} else if idx == 0 {
+		list.Prepend(v)
+		return nil
+	}
+	node := ListNode{Value: v}
+	curr := list.Head
+	for i := 0; i < idx; i++ {
+		curr = curr.Next
+	}
+	prev := curr.Prev
+	prev.Next = &node
+	node.Prev = prev
+	node.Next = curr
+	curr.Prev = &node
+	list.Length += 1
+	return nil
+}
+
+func (list *LinkedList) Append(value int) {
+	node := &ListNode{Value: value}
+	if list.Length == 0 {
+		list.Head, list.Tail = node, node
+	} else {
+		prev := list.Tail
+		prev.Next = node
+		node.Prev = prev
+		list.Tail = node
+	}
+	list.Length += 1
+}
+
+func (list *LinkedList) Get(idx int) (*int, error) {
+	if idx > list.Length-1 {
+		return nil, fmt.Errorf("can't Get, idx %v is unavailable", idx)
+	}
+	current := list.Head
+	for i := 0; i < idx; i++ {
+		current = current.Next
+	}
+	return &current.Value, nil
+}
+
+func (list *LinkedList) remove(node *ListNode) *int {
+	prev, next := node.Prev, node.Next
+	if list.Length == 1 { // 1 node list
+		list.Head, list.Tail = nil, nil
+	} else if prev == nil { // head
+		list.Head = next
+	} else if next == nil {
+		list.Tail = prev
+	} else {
+		prev.Next = next
+		next.Prev = prev
+	}
+	list.Length--
+	return &node.Value
+}
+
+func (list *LinkedList) RemoveAt(idx int) (*int, error) {
+	if idx > list.Length-1 {
+		return nil, fmt.Errorf("can't RemoveAt, idx %v is unavailable", idx)
+	}
+	curr := list.Head
+	for i := 0; i < idx; i++ {
+		curr = curr.Next
+	}
+	return list.remove(curr), nil
+}
+
+func (list LinkedList) String() string {
+	if list.Head == nil {
 		return "(Empty LinkedList)"
 	}
-	current := ll.Head
+	current := list.Head
 	result := ""
 	for current.Next != nil {
 		result += fmt.Sprintf("(%v)-", current.Value)
@@ -28,71 +116,4 @@ func (ll LinkedList) String() string {
 	}
 	result += fmt.Sprintf("(%v)", current.Value)
 	return result
-}
-
-func (ll *LinkedList) Prepend(value int) {
-	node := &ListNode{Value: value}
-	if ll.Length == 0 {
-		ll.Head = node
-		ll.Tail = node
-	} else {
-		postHead := ll.Head
-		postHead.Prev = node
-		node.Next = postHead
-		ll.Head = node
-	}
-	ll.Length += 1
-}
-
-func (ll *LinkedList) Append(value int) {
-	node := &ListNode{Value: value}
-	if ll.Length == 0 {
-		ll.Head = node
-		ll.Tail = node
-	} else {
-		preTail := ll.Tail
-		preTail.Next = node
-		node.Prev = preTail
-		ll.Tail = node
-	}
-	ll.Length += 1
-}
-
-func (ll *LinkedList) Get(idx int) (*ListNode, error) {
-	if idx > ll.Length-1 {
-		return nil, fmt.Errorf("idx %v is unavailable", idx)
-	}
-	current := ll.Head
-	for i := 0; i < idx; i++ {
-		current = current.Next
-	}
-	return current, nil
-}
-
-func (ll *LinkedList) Remove(ln *ListNode) *ListNode {
-	lnPrev, lnNext := ln.Prev, ln.Next
-
-	if lnPrev != nil {
-		lnPrev.Next = lnNext
-		if lnPrev.Next == nil {
-			ll.Tail = lnPrev
-		}
-	}
-
-	if lnNext != nil {
-		lnNext.Prev = lnPrev
-		if lnNext.Prev == nil {
-			ll.Head = lnPrev
-		}
-	}
-	// if lnNext.Next == n
-
-	return ln
-}
-func (ll *LinkedList) RemoveAt(idx int) (*ListNode, error) {
-	ln, err := ll.Get(idx)
-	if err != nil {
-		return nil, err
-	}
-	return ll.Remove(ln), nil
 }
--- linked-list_test.go
+++ linked-list_test.go
@@ -10,16 +10,44 @@ import (
 
 func TestLinkedList(t *testing.T) {
 
-	ll := algorithms.LinkedList{}
+	list := algorithms.LinkedList{}
 
-	ll.Append(102)
-	ll.Append(103)
-	ll.Prepend(101)
-	ll.Prepend(100)
-	fmt.Println(ll)
-	assert.Equal(t, ll.Length, 4)
+	list.Append(5)
+	list.Append(7)
+	list.Append(9)
+	fmt.Println(list)
 
-	node, _ := ll.Get(3)
-	assert.Equal(t, node.Value, 103)
+	v, _ := list.Get(2)
+	assert.Equal(t, 9, *v)
+
+	v, _ = list.RemoveAt(1)
+	assert.Equal(t, 7, *v)
+	assert.Equal(t, 2, list.Length)
+	fmt.Println(list)
+
+	list.Append(11)
+	v, _ = list.RemoveAt(1)
+	assert.Equal(t, 9, *v)
+	v, _ = list.RemoveAt(0)
+	assert.Equal(t, 5, *v)
+	v, _ = list.RemoveAt(0)
+	assert.Equal(t, 11, *v)
+	assert.Equal(t, 0, list.Length)
+	fmt.Println(list)
+
+	list.Prepend(5)
+	list.Prepend(7)
+	list.Prepend(9)
+	v, _ = list.Get(2)
+	assert.Equal(t, 5, *v)
+	v, _ = list.Get(0)
+	assert.Equal(t, 9, *v)
+	fmt.Println(list)
+
+	list.RemoveAt(0)
+	assert.Equal(t, 2, list.Length)
+	v, _ = list.Get(0)
+	assert.Equal(t, 7, *v)
+	fmt.Println(list)
 
 }
